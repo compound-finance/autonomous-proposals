a0b9bcee5130ca096f491c07a24489728d3f8a25
Antonina Norair <antonina.norair@gmail.com>
2020-09-14 16:24:06 -0700

    Add linter and code coverage

--- saddle.config.js
+++ saddle.config.js
@@ -4,6 +4,8 @@ module.exports = {
   // solc_args: [],                                        // Extra solc args
   build_dir: process.env['SADDLE_BUILD'] || ".build",      // Directory to place built contracts
   contracts: process.env['SADDLE_CONTRACTS'] || "contracts/*.sol tests/contracts/*.sol",  // Glob to match contract files
+  // coverage_dir: "coverage",                              // Directory to place coverage files
+  // coverage_ignore: [],
   tests: ['**/tests/*Test.js'],                         // Glob to match test files
   networks: {                                           // Define configuration for each network
     ropsten: {
